f0b1a39f860105fc87c3dd5cdcff0037d2cdd6ec
Travis Baka <tbaka@akamai.com>
2023-12-13 12:58:48 -0600

    remove provision.yml from deploy

--- deployment_scripts/linode-marketplace-kali/kali-deploy.sh
+++ deployment_scripts/linode-marketplace-kali/kali-deploy.sh
@@ -115,7 +115,7 @@ function run {
   # populate group_vars
   udf
   # run playbooks
-  for playbook in provision.yml site.yml; do ansible-playbook -v $playbook; done
+  for playbook in site.yml; do ansible-playbook -v $playbook; done
 }
 
 # main
